d92c9bbdb9ef92a1e3e3aaaaa7f98aa0d27afc70
chosungwon <qqwow741@gmail.com>
2022-04-02 23:41:35 +0900

    add bringDosHeader function

--- main.c
+++ main.c
@@ -3,12 +3,25 @@
 
 #define path "C:\\Windows\\notepad.exe"
 
+
+int bringDosHeader(unsigned char binary[]){
+
+    printf("이 파일의 e_magic = %X%X = %c%c\n", binary[0], binary[1], binary[0], binary[1]);
+    printf("이 파일의 e_lfanew = ");
+    for (int i = 57; i <= 60 ; i++) {
+        printf("%02X", binary[i]);
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[]){ //나중에는 파일 경로를 argument를 통해 받는걸로 변경.
+    printf("파일의 경로 : %s\n", path);
     FILE* fp = NULL;
     int size = 0;
-    char *binary_buf = NULL;
+    unsigned char *binary_buf = NULL; //이진수를 넣어야해서 unsigned char로 선언해야함
 
-    fp = fopen(path, "rb"); //argv[1]
+    fp = fopen(path, "rb"); //argv[1]를 통해 받는걸로 구현
     if (fp == NULL){
         printf("File Open error\n");
         return 1;
@@ -22,9 +35,7 @@ int main(int argc, char *argv[]){ //나중에는 파일 경로를 argument를 통해 받는걸�
     fread(binary_buf, 1, size, fp);
     fclose(fp);
 
-
-    printf("%c", binary_buf[0]);
-    printf("%c", binary_buf[1]);
+    bringDosHeader(binary_buf);
 
 
     return 0;
